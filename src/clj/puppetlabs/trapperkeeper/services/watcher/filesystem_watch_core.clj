1b897dc86276e42c48a12a3850fc18221d7b85e9
Justin Stoller <justin.stoller@gmail.com>
2016-08-06 09:10:07 -0700

    (TK-389) Various improvemnts based on PR review:
      - typo, watiting -> waiting
      - add schema to get-event-maps-from-key
      - improve info logging

--- src/clj/puppetlabs/trapperkeeper/services/watcher/filesystem_watch_core.clj
+++ src/clj/puppetlabs/trapperkeeper/services/watcher/filesystem_watch_core.clj
@@ -98,14 +98,14 @@
                                           (filter dir-create?)
                                           (map #(.toPath (:full-path %)))))))
 
-(defn get-event-maps-from-key
-  [watch-key]
+(schema/defn get-event-maps-from-key :- [Event]
+  [watch-key :- WatchKey]
   (let [events (.pollEvents watch-key)]
     (map #(clojurize % (.watchable watch-key)) events)))
 
 (schema/defn retrieve-events :- [Event]
   "Blocks until an event the watcher is concerned with has occured. Will then
-  poll for a new event, watiting at least `window-min` for a new event to
+  poll for a new event, waiting at least `window-min` for a new event to
   occur. Will continue polling for as long as there are new events that occur
   within `window-min`, or the `window-max` time limit has been exceeded."
   [watcher :- (schema/protocol Watcher)]
@@ -129,9 +129,11 @@
   "Process for side-effects any events that occured for watcher's watch-key"
   [watcher :- (schema/protocol Watcher)
    events :- [Event]]
-  (let [callbacks @(:callbacks watcher)]
-    (log/info (trs "Got {0} event(s) on path(s) {1}"
-                   (count events) (distinct (map #(:full-path %) events))))
+  (let [callbacks @(:callbacks watcher)
+        events-by-dir (group-by :watched-path events)]
+    (doseq [[dir events'] events-by-dir]
+      (log/info (trs "Got {0} event(s) in directory {1}"
+                   (count events') dir)))
     (log/debugf "%s\n%s"
                 (trs "Events:")
                 (ks/pprint-to-string
